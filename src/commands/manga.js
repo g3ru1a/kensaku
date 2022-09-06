0f7820c191b8ef0cdcace2dcef8e884c9b9e64cb
G3ru1a <egidiufarcas@maze.ws>
2022-09-06 19:29:13 +0100

    manga sort

--- src/commands/manga.js
+++ src/commands/manga.js
@@ -5,7 +5,7 @@ const MangaEmbed = require("../embeds/manga");
 
 const query = `
 query ($search_query: String) { 
-  Media (search: $search_query, type: MANGA) {
+  Media (search: $search_query, type: MANGA, sort: FORMAT) {
     id,
     idMal,
     title {
