74659765061ec6adefc83962f37e2392d4739252
Ed <egidiufarcas@maze.ws>
2023-04-25 15:39:16 +0100

    Create inviteCMD.js
    added a command to give an invite link

---
+++ src/commands/inviteCMD.js
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonStyle } from "discord.js";
+import MangaEmbed from "../embeds/manga.js";
+import { Buttons } from "../helpers/buttons.js";
+import Media from "../helpers/media.js";
+import { ThreadFollow } from "../helpers/threadFollow.js";
+
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("kinvite")
+        .setDescription("DM's you an invite link for the bot!"),
+    async execute(interaction) {
+        interaction.member.send({content: "Use the link below to add me to your server: https://discord.com/oauth2/authorize?client_id=1016677293789298729&scope=bot&permissions=414464724032"});
+    },
+};
