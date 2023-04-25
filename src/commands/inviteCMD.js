6bf7ad0f83ea4ce52d367678e67afd9120b185f7
Ed <egidiufarcas@maze.ws>
2023-04-25 15:47:11 +0100

    Update inviteCMD.js

--- src/commands/inviteCMD.js
+++ src/commands/inviteCMD.js
@@ -10,6 +10,7 @@ export default {
         .setName("kinvite")
         .setDescription("DM's you an invite link for the bot!"),
     async execute(interaction) {
-        interaction.member.send({content: "Use the link below to add me to your server: https://discord.com/oauth2/authorize?client_id=1016677293789298729&scope=bot&permissions=414464724032"});
+        interaction.member.send({content: "Use the following link to add me to your server: https://discord.com/oauth2/authorize?client_id=1016677293789298729&scope=bot&permissions=414464724032"});
+        interaction.reply({content: "DM'd the link to you.", ephemeral: true});
     },
 };
